487fafa19cf4d54c20a24c65f97568aeb5e82669
yonvela20 <yon.vela20@gmail.com>
2024-03-04 13:13:25 +0100

    First commit! Added spring project and rabbitmq docker-compose

---
+++ amplia/src/main/java/com/micro/streaming/analytics/amplia/AmpliaApplication.java
@@ -0,0 +1,13 @@
+package com.micro.streaming.analytics.amplia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmpliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmpliaApplication.class, args);
+	}
+
+}
---
+++ amplia/src/test/java/com/micro/streaming/analytics/amplia/AmpliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.streaming.analytics.amplia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmpliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
